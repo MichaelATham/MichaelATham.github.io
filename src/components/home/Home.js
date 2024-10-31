db803695e3741fc1c57270206d862cd5d274ff28
Michael <michaelandrewtham@gmail.com>
2024-10-31 11:48:30 -0400

    testing UI stuff

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import AltHome from "./AltHome";
